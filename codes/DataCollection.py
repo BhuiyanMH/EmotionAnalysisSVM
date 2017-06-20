4277f26315b52f9639020337fdcd9133f5a65950
ASUS <rony.cse.cu.13@gmaail.com>
2017-06-20 22:59:42 +0600

    Final Commit of the Completed project

--- codes/DataCollection.py
+++ codes/DataCollection.py
@@ -7,23 +7,24 @@ consumerKey = 'wb22Y8wyJ5dQZIqcuQQs3TOMr'
 consumerSecret = 'sefxdoiTHWCrEBwSWE9zCPSb4VqTIwXe95fCTBk6qi2Y34yor3'
 accessToken = '836485825353277440-K6JwZiLRgwtwYlWUIwnykoszckQ08Ps'
 accessSecret = 'WFOFTLkaMPLPQoc160RhBKhleI7YmAFXQ0RJzEcOUSP5j'
-#anger, disgust, fear, guilt, interest, joy, sadness, shame and surprise
+#Emotion Categories: anger, disgust, fear, guilt, interest, joy, sadness, shame and surprise
 
 class listener(StreamListener):
     def on_data(self, data):
         try:
             #split the tweet and take only text portion of the tweet
             tweet_text = data.split(',"text":"')[1].split('","source')[0]#[1] indicate the right side of the split
-            #display the tweet in the console
-            print(tweet_text)
-            #add timestamp to the tweet
-            tweet_save = str(time.time())+'::'+tweet_text
-            #open file in append mode for saving tweet
-            saving_file = open('../data/twitDBJoy.csv', 'a')
-            saving_file.write(tweet_save)
-            saving_file.write('\n')
-            saving_file.close()
-            return True
+
+
+            #Skipping Re-Tweets and tweets containing links to other content
+            if "RT @" not in tweet_text and "https:" not in tweet_text \
+                    and "http:" not in tweet_text and "\\u" not in tweet_text:
+                print(tweet_text)
+                saving_file = open('../data/tweets', 'a')
+                saving_file.write(tweet_text)
+                saving_file.write('\n')
+                saving_file.close()
+                return True
         except BaseException as e:
             print('Failed on data', str(e))
             time.sleep(5)
@@ -35,4 +36,6 @@ auth = OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessSecret)
 twitterStream = Stream(auth, listener())
 #filter tweet according to keyword
-twitterStream.filter(track=["joy"])
+twitterStream.filter(track=["sad"])
+
+
