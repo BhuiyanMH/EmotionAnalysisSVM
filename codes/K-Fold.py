f4d907befb0b400df416417202cb6e93e3338874
ASUS <rony.cse.cu.13@gmaail.com>
2017-06-20 23:02:29 +0600

    Final Commit of the Completed project

---
+++ codes/K-Fold.py
@@ -0,0 +1,106 @@
+#importing necessary modules
+import csv
+import numpy as np
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+
+#creating stemmer for stemming
+ps = PorterStemmer()
+categories = ["neutral","anger", "disgust", "fear", "Guilt", "Interest", "Joy", "Sad", "shame", "surprise"]
+stopwords = stopwords.words("english")
+
+#text will contain the tweets
+text = []
+#cat will contain the emotion category of tweets
+cat =[]
+
+#reading data from csv file
+with open("../data/tweets.csv") as tweet:
+    reader = csv.reader(tweet)
+    for row in reader:
+        text.append(row[0])
+        cat.append(row[1])
+
+#Preprocessing task
+temp = []
+for i in range(4000):
+    temp=word_tokenize(text[i], language="english")
+    text[i] = ""
+    for j in range(len(temp)):
+        if temp[j] not in stopwords:
+            temp[j] = ps.stem(temp[j])
+            text[i] += temp[j] + " "
+
+def k_fold(foldNum):
+
+    print("K-Fold iteration: " , foldNum)
+    #Building the pipeline for sequential tasks
+    #This pipeline will first vectorize the text as Bag of Words
+    #  then find the IF_IDF of the terms
+    text_classifier = Pipeline([('vectorizer', CountVectorizer()),
+                            ('tfidfFinder', TfidfTransformer()),
+                            ('classifier', MultinomialNB()),])
+    #creating the classifier
+    text_clf = text_classifier.fit(trainText, trainCat)
+
+    #Evaluation
+    doc_test = testText
+    predicted  = text_clf.predict(testText)
+
+    #printing classification accuracy
+    print("\nNaive Bayesian Classifier accuracy: ",np.mean(predicted == testCat)*100,"%")
+    #printing classification summary
+    print("Classification summary for NB Classifier:\n")
+    print(metrics.classification_report(testCat, predicted, target_names=categories))
+    #printing confusin matrix
+
+    #Neural Neteork Classifier
+    text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),
+                     ('clf', MLPClassifier(hidden_layer_sizes=(13,13,13),
+                                           max_iter=1000)),])
+    text_clf= text_clf.fit(trainText, trainCat)
+    predicted = text_clf.predict(testText)
+    print("\nNN Accuracy: ",np.mean(predicted == testCat)*100,"%")
+
+    print("Classification summary for ANN:\n")
+    print(metrics.classification_report(testCat, predicted, target_names=categories))
+
+    #using Support vector machine
+    text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),
+                     ('clf', SGDClassifier(loss='hinge', penalty='l2',
+                        alpha=1e-3, n_iter=5, random_state=42)),])
+    text_clf= text_clf.fit(trainText, trainCat)
+    predicted = text_clf.predict(testText)
+    print("\nSVM Accuracy: ",np.mean(predicted == testCat)*100,"%")
+
+    print("Classification summary for SVM:\n")
+    print(metrics.classification_report(testCat, predicted, target_names=categories))
+
+startIndex = 0
+endIndex = 400
+
+for i in range(1, 11):
+    trainText = []
+    trainCat = []
+    testText = []
+    testCat = []
+    for j in range(4000):
+        if j not in range(startIndex, endIndex):
+            trainText.append(text[j])
+            trainCat.append(cat[j])
+    testText = text[startIndex:endIndex]
+    testCat= cat[startIndex:endIndex]
+    k_fold(i)
+    startIndex+=400
+    endIndex+=400
+
+
+
